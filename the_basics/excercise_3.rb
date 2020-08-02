45407476d0e6c3ffcdf19a4b87eadd3774f0b5bb
hiimchinh <chinhhm@stringee.com>
2020-08-02 17:40:08 +0700

    add exercise 3

---
+++ the_basics/excercise_3.rb
@@ -0,0 +1,13 @@
+movies = {
+  dog_day_afternoon: 1975,
+  troy: 2004,
+  the_wolf_of_wall_street: 2013,
+  harry_potter_and_the_sorcerers_stone: 2001,
+  raiders_of_the_lost_ark: 1981
+}
+
+puts movies[:dog_day_afternoon]
+puts movies[:troy]
+puts movies[:the_wolf_of_wall_street]
+puts movies[:harry_potter_and_the_sorcerers_stone]
+puts movies[:raiders_of_the_lost_ark]
