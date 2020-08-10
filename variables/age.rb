b3357081f77b720bdc1541d26c9725373a5ac5ca
hiimchinh <chinhhoang1196@gmail.com>
2020-08-10 11:52:21 +0700

    add 2 first excercise variable section

---
+++ variables/age.rb
@@ -0,0 +1,15 @@
+puts "How old are you ?"
+age = gets.chomp.to_i
+
+age += 10
+puts "In 10 years you will be"
+puts age
+age += 10
+puts "In 20 years you will be"
+puts age
+age += 10
+puts "In 30 years you will be"
+puts age
+age += 10
+puts "In 40 years you will be"
+puts age
---
+++ variables/name.rb
@@ -0,0 +1,3 @@
+puts "Enter your name please?"
+name = gets.chomp
+puts "Your name is #{name}"
