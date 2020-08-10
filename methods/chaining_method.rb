9a086eb0b13eb64d0abd8ce96338eb13c2cbf0e9
hiimchinh <chinhhoang1196@gmail.com>
2020-08-10 18:08:51 +0700

    add stuff from methods

---
+++ methods/chaining_method.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+def add_three(number)
+  number + 3
+end
+
+add_three(5).times { puts 'this should print 8 times' }
+
---
+++ methods/greeting.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+  puts "Greetings my friend #{name}"
+end
+
+greeting('chinh')
