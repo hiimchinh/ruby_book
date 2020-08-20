396f3fa5089e9f20b5ab21523aa0e024423fe833
hiimchinh <chinhhoang1196@gmail.com>
2020-08-20 17:50:31 +0700

    add excercise 5,6,7

---
+++ hashes/5.rb
@@ -0,0 +1,6 @@
+test_hashes = {
+  number: 54,
+  string: 'stringee.com',
+  array: [1,2]
+}
+puts test_hashes.value?(54)
---
+++ hashes/6.rb
@@ -0,0 +1,7 @@
+x = "hi there"
+
+# this hash has a symbol x as a key
+my_hash = {x: "some value"}
+
+# this hash has a string "hi there" as a key
+my_hash2 = {x => "some value"}
