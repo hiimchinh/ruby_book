36ca82b30f3bcf62d2967e5400c0c531f48bc55e
hiimchinh <chinhhoang1196@gmail.com>
2020-08-24 01:20:21 +0700

    add last excercise

--- Excercies/16.rb
+++ Excercies/16.rb
@@ -1,2 +1,10 @@
 contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
 contacts = {"Joe Smith" => {}}
+data_keys = [:email, :address, :phone]
+contacts.each do |name, value|
+  data_keys.each do |key|
+    value[key] = contact_data.shift
+  end
+end
+
+puts contacts
